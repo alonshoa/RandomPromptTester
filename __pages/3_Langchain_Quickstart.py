2679e876fc5cb87203bd2c7983a162ce735d8681
Alon <shoa.alon@gmail.com>
2025-05-11 17:36:00 +0300

    Initial commit

---
+++ __pages/3_Langchain_Quickstart.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from langchain.llms import OpenAI
+
+st.title("🦜🔗 Langchain Quickstart App")
+
+with st.sidebar:
+    user_id = st.text_input("User ID", type="default")
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+
+with st.form("my_form"):
+    if not user_id:
+        st.info("Please add your User ID.")
+    text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+    elif submitted:
+        generate_response(text)
